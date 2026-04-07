f7c8abe7cc09f2a2bd9d2978c7d162308dc1dac1
Link4Electronics <link4electronics@gmail.com>
2026-04-07 07:20:16 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -8,9 +8,6 @@ export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=https://raw.githubusercontent.com/skynettx/raptor/refs/heads/master/rsrc/raptor.png
-export DESKTOP=DUMMY
-export MAIN_BIN=raptor
-export STARTUPWMCLASS=raptor
 export DEPLOY_OPENGL=1
 export DEPLOY_PIPEWIRE=1
 
