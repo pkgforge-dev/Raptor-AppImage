ccc8737605b09506f808179f1a2a2e19262cb000
Link4Electronics <link4electronics@gmail.com>
2026-08-15 06:18:03 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -13,8 +13,6 @@ export DEPLOY_PULSE=1
 
 # Deploy dependencies
 quick-sharun ./AppDir/bin /usr/lib/libudev.so* /usr/lib/libusb-1.0.so*
-
-# Additional changes can be done in between here
 echo 'ANYLINUX_DO_NOT_LOAD_LIBS=libpipewire-0.3.so*:${ANYLINUX_DO_NOT_LOAD_LIBS}' >> ./AppDir/.env
 
 # Turn AppDir into AppImage
