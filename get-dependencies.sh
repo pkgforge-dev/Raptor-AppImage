eaa715b98968a2d171637ed85a2dc95f0fdacf82
Link4Electronics <link4electronics@gmail.com>
2026-08-15 06:17:51 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -15,11 +15,6 @@ echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
-# Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
-
-# If the application needs to be manually built that has to be done down here
-# if [ "${DEVEL_RELEASE-}" = 1 ]; then
 echo "Making nightly build of Raptor..."
 echo "---------------------------------------------------------------"
 REPO="https://github.com/skynettx/raptor"
